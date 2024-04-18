38615ed1b14739f337d6e75e8856ca05aa64e784
MACKOPRZYBYTEK <murzynek123321@gmail.com>
2024-04-18 12:50:48 +0200

    feat: added read funcionality

---
+++ models/data.py
@@ -0,0 +1,7 @@
+users: list[dict] = [
+    {"name": "Maciej", "surname": "Przybytek", "posts": 20},
+    {"name": "Jan", "surname": "Mielec", "posts": 45},
+    {"name": "Bartosz", "surname": "Pietrasik", "posts": 60},
+    {"name": "Tymoteusz", "surname": "Miszczak", "posts": 21},
+    {"name": "Mateusz", "surname": "Matysiak", "posts": 33},
+]
