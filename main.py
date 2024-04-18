76ffff05ac77f86e69c5ee7c9a81f57ab233d766
MACKOPRZYBYTEK <murzynek123321@gmail.com>
2024-04-18 14:25:24 +0200

    feat: added search user

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from utiles.crud import read, create_user
+from utiles.crud import read, create_user, search
 from models.data import users
-
-
 
 if __name__ == '__main__':
 
@@ -11,6 +9,7 @@ if __name__ == '__main__':
         print("0. Zakończ program: ")
         print("1.Pokaż co u znajomych: ")
         print("2.Dodaj znajomego: ")
+        print("3. Wyszukaj znajomego: ")
         menu_option: str = input("Wybierz dostępną funkcję z menu: ")
         if menu_option == "0":
             break
@@ -18,7 +17,8 @@ if __name__ == '__main__':
             read(users)
         if menu_option == "2":
             create_user(users)
-
+        if menu_option == "3":
+            search(users)
 
 
 
