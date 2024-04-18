307e92f40a12f6035883f2af398ae948f52f7872
MACKOPRZYBYTEK <murzynek123321@gmail.com>
2024-04-18 13:59:03 +0200

    feat: added create user

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from utiles.crud import read
+from utiles.crud import read, create_user
 from models.data import users
+
 
 
 if __name__ == '__main__':
@@ -9,26 +10,20 @@ if __name__ == '__main__':
         print("menu:")
         print("0. Zakończ program: ")
         print("1.Pokaż co u znajomych: ")
+        print("2.Dodaj znajomego: ")
         menu_option: str = input("Wybierz dostępną funkcję z menu: ")
         if menu_option == "0":
             break
         if menu_option == "1":
             read(users)
+        if menu_option == "2":
+            create_user(users)
 
 
 
 
 
-print(f"Witaj {users[0]['name']}!")
-while True:
-    print("menu:")
-    print("0. Zakończ program: ")
-    print("1.Pokaż co u znajomych: ")
-    menu_option:str=input("Wybierz dostępną funkcję z menu: ")
-    if menu_option == "0":
-        break
-    if menu_option == "1":
-        read(users)
+
 
 
 
